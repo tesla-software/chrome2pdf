fecab508a9800c716c43ae6e1a771da7dd39688f
Karlo Mikus <karlo.mikus1@gmail.com>
2019-07-08 11:30:26 +0200

    Update tests

--- tests/Chrome2PdfTest.php
+++ tests/Chrome2PdfTest.php
@@ -1,14 +1,29 @@
 <?php
 namespace Tesla\Chrome2Pdf;
 
+use Smalot\PdfParser\Parser;
+
 class Chrome2PdfTest extends TestCase
 {
+    public function setUp(): void
+    {
+        $this->emptyTempDirectory();
+    }
+
     /** @test */
     public function it_generates_pdf_with_content()
     {
         $pdf = (new Chrome2Pdf())->setChromeExecutablePath('google-chrome-stable')->setContent('<h1>Test</h1>')->pdf();
+        $filename = __DIR__ . '/temp/test.pdf';
+
+        file_put_contents($filename, $pdf);
+
+        $parser = new Parser();
+        $createdPdf = $parser->parseFile($filename);
 
         $this->assertNotNull($pdf);
+        $this->assertEquals('Test', $createdPdf->getText());
+        $this->assertCount(1, $createdPdf->getPages());
     }
 
     /** @test */
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,4 +5,14 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
+    protected function emptyTempDirectory()
+    {
+        $tempDirPath = __DIR__ . '/temp';
+        $files = scandir($tempDirPath);
+        foreach ($files as $file) {
+            if (! in_array($file, ['.', '..', '.gitignore'])) {
+                unlink("{$tempDirPath}/{$file}");
+            }
+        }
+    }
 }
